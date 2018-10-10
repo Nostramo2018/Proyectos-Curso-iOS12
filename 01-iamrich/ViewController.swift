1edf3886dd8fa8104e875975e2a5416d86952a91
Nostramo <nostramo2018@icloud.com>
2018-10-10 10:03:15 +0200

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Nostramo on 10/10/2018.
+//  Copyright © 2018 AvSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
