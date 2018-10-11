4b9b37a370fbf63ed89945b032ae58548aa3aa41
Nostramo <nostramo2018@icloud.com>
2018-10-11 11:38:11 +0200

    Proyecto 1 (I Am Rich) finalizado.

--- 01-iamrich/ViewController.swift
+++ 01-iamrich/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnPress(_ sender: UIButton) {
+        
+        let alertPress = UIAlertController(title: "I Am Rich!!", message: "I am rich.\nI deserve it.\nI am good,\nhealthy and successfull.", preferredStyle: .alert)
+        
+        let alertPressAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertPress.addAction(alertPressAction)
+        
+        self.show(alertPress, sender: nil)
+        
+    }
+    
+    
 
 }
 
